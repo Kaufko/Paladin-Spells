ee79a9f4aa225f202fe9040861ab993c74fdfde9
Kaufko <99084056+Kaufko@users.noreply.github.com>
2026-06-30 12:10:44 +0200

    Added effect for cosmetic purposes.

---
+++ src/main/java/eu/kaufko/paladin_spells/effects/BedrockSkinEffect.java
@@ -0,0 +1,10 @@
+package eu.kaufko.paladin_spells.effects;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class BedrockSkinEffect extends MobEffect {
+    public BedrockSkinEffect() {
+        super(MobEffectCategory.BENEFICIAL, 0xFFD700);
+    }
+}
---
+++ src/main/java/eu/kaufko/paladin_spells/effects/SwornProtectorEffect.java
@@ -0,0 +1,10 @@
+package eu.kaufko.paladin_spells.effects;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class SwornProtectorEffect extends MobEffect {
+    public SwornProtectorEffect() {
+        super(MobEffectCategory.BENEFICIAL, 0xFFD700);
+    }
+}
