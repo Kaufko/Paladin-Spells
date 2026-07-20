e6f0f0b14d6e330bcade6af445eb8d7a7ded42c5
Kaufko <99084056+Kaufko@users.noreply.github.com>
2026-07-20 15:25:15 +0200

    Enhance BedrockSkinEffect with new attributes
    Add damage reduction key and modify effect behavior

--- src/main/java/eu/kaufko/paladin_spells/effects/BedrockSkinEffect.java
+++ src/main/java/eu/kaufko/paladin_spells/effects/BedrockSkinEffect.java
@@ -4,7 +4,39 @@ import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 
 public class BedrockSkinEffect extends MobEffect {
+
+    public static final String DAMAGE_REDUCTION_KEY = "bedrock_skin_reduction";
+
     public BedrockSkinEffect() {
         super(MobEffectCategory.BENEFICIAL, 0xFFD700);
+
+        addAttributeModifier(
+                Attributes.MOVEMENT_SPEED,
+                "4d4bbdf5-f6be-4a57-94c9-d3ac4ae7d157",
+                -1.0D,
+                MobEffect.AttributeModifierOperation.ADD_MULTIPLIED_TOTAL
+        );
+    }
+
+    @Override
+    public boolean applyEffectTick(LivingEntity entity, int amplifier) {
+        entity.setDeltaMovement(0, entity.getDeltaMovement().y(), 0);
+        return true;
+    }
+
+    @Override
+    public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+        return true;
+    }
+
+    @Override
+    public void removeAttributeModifiers(
+            LivingEntity entity,
+            net.minecraft.world.entity.ai.attributes.AttributeMap attributes,
+            int amplifier
+    ) {
+        super.removeAttributeModifiers(entity, attributes, amplifier);
+
+        entity.getPersistentData().remove(DAMAGE_REDUCTION_KEY);
     }
 }
