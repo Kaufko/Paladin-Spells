31f64ccdb39d062b21cc25d434cb62d6463b486e
Kaufko <99084056+Kaufko@users.noreply.github.com>
2026-08-16 12:53:10 +0200

    Fixed server crash due to using client only class

---
+++ src/main/java/eu/kaufko/paladin_spells/client/SwornProtectorClient.java
@@ -0,0 +1,17 @@
+package eu.kaufko.paladin_spells.client;
+
+import eu.kaufko.paladin_spells.spells.SwornProtectorSpell;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.LivingEntity;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class SwornProtectorClient {
+    public static float getRedirect(SwornProtectorSpell spell, int spellLevel) {
+        LivingEntity player = Minecraft.getInstance().player;
+        if (player == null) return 0f;
+        return spell.getRedirectPercentage(spellLevel, SwornProtectorSpell.MAX_LEVEL, player);
+    }
+}
+
